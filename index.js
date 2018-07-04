dc24ed229111b473476e9b739d0039650188bfd1
ejskipper <ellen.skipper@btinternet.com>
2018-07-04 11:37:20 +0100

    Added a simple multiplier

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 console.log('Welcome to the calculator!');
+const readline = require('readline-sync');
+
+console.log('Please enter first number:');
+const response1=readline.prompt();
+
+console.log('Please enter second number:');
+const response2=readline.prompt();
+
+var answ = parseFloat(response1)*parseFloat(response2);
+console.log(response1, 'multiplied by', response2, 'is equal to', answ)
