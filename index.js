b64b9cd2073ef338eef4e7130ba826223d497606
ejskipper <ellen.skipper@btinternet.com>
2018-07-05 10:15:23 +0100

    Use +__ instead of parseFloat

--- index.js
+++ index.js
@@ -5,43 +5,51 @@ console.log('Please enter operator:');
 const operator=readline.prompt();
 
 if (operator=='*') {
-console.log('Please enter first number:');
-const response1=readline.prompt();
-
-console.log('Please enter second number:');
-const response2=readline.prompt();
-
-var answ = parseFloat(response1)*parseFloat(response2);
+    console.log('Please enter first number:');
+    const response1=readline.prompt();
+    const number1 = +response1;
+    
+    console.log('Please enter second number:');
+    const response2=readline.prompt();
+    const number2 = +response2;
+    
+    var answ = number1 * number2;
 console.log(response1, 'multiplied by', response2, 'is equal to', answ);
 
 } else if (operator=='+') {
 console.log('Please enter first number:');
 const response1=readline.prompt();
+const number1 = +response1;
 
 console.log('Please enter second number:');
 const response2=readline.prompt();
+const number2 = +response2;
 
-var answ = parseFloat(response1)+parseFloat(response2);
+var answ = number1 + number2;
 console.log(response1, 'added to', response2, 'is equal to', answ);
     
 } else if (operator=='-') {
-console.log('Please enter first number:');
-const response1=readline.prompt();
-
-console.log('Please enter second number:');
-const response2=readline.prompt();
-
-var answ = parseFloat(response1)-parseFloat(response2);
+    console.log('Please enter first number:');
+    const response1=readline.prompt();
+    const number1 = +response1;
+    
+    console.log('Please enter second number:');
+    const response2=readline.prompt();
+    const number2 = +response2;
+    
+    var answ = number1 - number2;
 console.log(response1, 'minus', response2, 'is equal to', answ);
 
 } else if (operator=='/') {
     console.log('Please enter first number:');
 const response1=readline.prompt();
+const number1 = +response1;
 
 console.log('Please enter second number:');
 const response2=readline.prompt();
+const number2 = +response2;
 
-var answ = parseFloat(response1)/parseFloat(response2);
+var answ = number1 / number2;
 console.log(response1, 'divided by', response2, 'is equal to', answ);
 
 } else {
