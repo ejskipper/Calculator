c02aa0efa8fec69f5665a36a60d365fb8e342d6f
ejskipper <ellen.skipper@btinternet.com>
2018-07-05 10:14:24 +0100

    Added all operators

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 console.log('Welcome to the calculator!');
 const readline = require('readline-sync');
 
+console.log('Please enter operator:');
+const operator=readline.prompt();
+
+if (operator=='*') {
 console.log('Please enter first number:');
 const response1=readline.prompt();
 
@@ -8,4 +12,38 @@ console.log('Please enter second number:');
 const response2=readline.prompt();
 
 var answ = parseFloat(response1)*parseFloat(response2);
-console.log(response1, 'multiplied by', response2, 'is equal to', answ)
+console.log(response1, 'multiplied by', response2, 'is equal to', answ);
+
+} else if (operator=='+') {
+console.log('Please enter first number:');
+const response1=readline.prompt();
+
+console.log('Please enter second number:');
+const response2=readline.prompt();
+
+var answ = parseFloat(response1)+parseFloat(response2);
+console.log(response1, 'added to', response2, 'is equal to', answ);
+    
+} else if (operator=='-') {
+console.log('Please enter first number:');
+const response1=readline.prompt();
+
+console.log('Please enter second number:');
+const response2=readline.prompt();
+
+var answ = parseFloat(response1)-parseFloat(response2);
+console.log(response1, 'minus', response2, 'is equal to', answ);
+
+} else if (operator=='/') {
+    console.log('Please enter first number:');
+const response1=readline.prompt();
+
+console.log('Please enter second number:');
+const response2=readline.prompt();
+
+var answ = parseFloat(response1)/parseFloat(response2);
+console.log(response1, 'divided by', response2, 'is equal to', answ);
+
+} else {
+console.log('Invalid operator.')
+}
