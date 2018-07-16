1df6f955743502330b8abde12aa558a8e98301df
ejskipper <ellen.skipper@btinternet.com>
2018-07-16 23:52:49 +0100

    Removed redundant try..catch

--- arithmetic.js
+++ arithmetic.js
@@ -19,18 +19,15 @@ function enterNumber(promptText) {
 function getOperator() {
     console.log('Please enter operator:');
     var operator=readline.prompt();
-    try {
         if (operator=='+'||operator=='-'||operator=='/'||operator=='*') {
             return operator;
         }
+        
         else {
-            throw'Invalid Operator'
-        }
-    }
-    catch (e) {
         console.log(`"${operator}" is not a valid operator. Please enter a valid operator. (+, -, * or /)`)
         operator=getOperator();
-    }
+        }
+
     return operator;
 }
 exports.performOneArithmeticCalculation = function() {
